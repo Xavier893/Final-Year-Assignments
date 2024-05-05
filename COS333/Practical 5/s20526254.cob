66b4b7c78dc74e7df178188963588efbbbac9582
XavRyn <u20526254@tuks.co.za>
2024-05-05 18:35:23 +0200

    Added Specification and completed implementation for Fortran and COBOL

---
+++ COS333/Practical 5/s20526254.cob
@@ -0,0 +1,69 @@
+        IDENTIFICATION DIVISION. 
+        PROGRAM-ID. SIMPLESTATS. 
+
+        DATA DIVISION.
+          WORKING-STORAGE SECTION.
+            *> Comment
+            01 NUM-ELEMENTS PIC 9(2).
+            01 ARR PIC 9(5) OCCURS 100 TIMES.
+            01 SMALLEST PIC 9(5).
+            01 LARGEST PIC 9(5).
+            01 MODE-VAL PIC 9(5). 
+            01 I PIC 9(5).
+            01 J PIC 9(5).
+            01 COUNTER PIC 9(5). 
+            01 MAX-COUNT PIC 9(5).
+            01 CANDIDATE PIC 9(5). 
+
+        PROCEDURE DIVISION.
+            DISPLAY "How many integers would you like? "
+            ACCEPT NUM-ELEMENTS
+            
+            PERFORM READDATA.
+
+            PERFORM FINDSMALLEST.
+            PERFORM FINDLARGEST.
+            PERFORM MODEARR.
+
+            DISPLAY "Smallest value: " SMALLEST.
+            DISPLAY "Largest value: " LARGEST.
+            DISPLAY "Mode value: " MODE-VAL.
+            STOP RUN.
+
+        READDATA.
+            PERFORM VARYING I FROM 1 BY 1 UNTIL I > NUM-ELEMENTS
+                DISPLAY "Enter value " I ": " 
+                ACCEPT ARR(I)
+            END-PERFORM.
+
+        FINDSMALLEST.
+            MOVE ARR(1) TO SMALLEST.
+            PERFORM VARYING I FROM 2 BY 1 UNTIL I > NUM-ELEMENTS
+                IF ARR(I) < SMALLEST THEN
+                    MOVE ARR(I) TO SMALLEST
+                END-IF
+            END-PERFORM. 
+
+        FINDLARGEST.
+            MOVE ARR(1) TO LARGEST. 
+            PERFORM VARYING I FROM 2 BY 1 UNTIL I > NUM-ELEMENTS
+                IF ARR(I) > LARGEST THEN
+                    MOVE ARR(I) TO LARGEST
+                END-IF
+            END-PERFORM. 
+
+        MODEARR.
+            MOVE 0 TO MAX-COUNT.
+            PERFORM VARYING I FROM 1 BY 1 UNTIL I > NUM-ELEMENTS
+                MOVE 0 TO COUNTER
+                PERFORM VARYING J FROM 1 BY 1 UNTIL J > NUM-ELEMENTS
+                    IF ARR(I) = ARR(J) THEN
+                        ADD 1 TO COUNTER
+                    END-IF
+                END-PERFORM 
+                IF COUNTER > MAX-COUNT THEN
+                    MOVE COUNTER TO MAX-COUNT
+                    MOVE ARR(I) TO CANDIDATE 
+                END-IF
+            END-PERFORM.
+            MOVE CANDIDATE TO MODE-VAL.
---
+++ COS333/Practical 5/s20526254.f
@@ -0,0 +1,135 @@
+        program SimpleStats
+            implicit none
+            integer :: num_elements
+            integer, allocatable :: arr(:)
+            integer :: smallest, largest, mode_val
+
+            ! Interface block for readData subroutine
+            interface
+                subroutine readData(arr, num_elements)
+                    implicit none
+                    integer, intent(out), allocatable :: arr(:)
+                    integer, intent(in) :: num_elements
+                end subroutine readData
+            end interface
+
+            ! Interface block for findSmallest subroutine
+            interface
+                subroutine findSmallest(arr, smallest)
+                    implicit none
+                    integer, intent(in) :: arr(:)
+                    integer, intent(out) :: smallest
+                end subroutine findSmallest
+            end interface
+
+            ! Interface block for findLargest subroutine
+            interface
+                subroutine findLargest(arr, largest)
+                    implicit none
+                    integer, intent(in) :: arr(:)
+                    integer, intent(out) :: largest
+                end subroutine findLargest
+            end interface
+
+            ! Interface block for mode subroutine
+            interface
+                subroutine mode(arr, mode_val)
+                    implicit none
+                    integer, intent(in) :: arr(:)
+                    integer, intent(out) :: mode_val
+                end subroutine mode
+            end interface
+
+            ! Prompt the user to enter the number of elements
+            print *, "Enter the number of elements:"
+            read *, num_elements
+
+            ! Allocate the array based on user input size
+            allocate(arr(num_elements))
+
+            ! Call the readData subroutine
+            call readData(arr, num_elements)
+
+            ! Find the smallest and largest values
+            call findSmallest(arr, smallest)
+            call findLargest(arr, largest)
+
+            ! Find the mode
+            call mode(arr, mode_val)
+
+            ! Print the results
+            print *, "Smallest value:", smallest
+            print *, "Largest value:", largest
+            print *, "Mode value:", mode_val
+
+            ! Deallocate the array
+            deallocate(arr)
+        end program SimpleStats
+
+        ! Subprogram to read data into the array
+        subroutine readData(arr, num_elements)
+            implicit none
+            integer, intent(out), allocatable :: arr(:)
+            integer, intent(in) :: num_elements
+            integer :: i
+
+            allocate(arr(num_elements)) ! Allocate the array based on the input size
+
+            do i = 1, num_elements
+                print *, "Enter value ", i, ":"
+                read *, arr(i)
+            end do
+        end subroutine readData
+
+        ! Subprogram to find the smallest value in the array
+        subroutine findSmallest(arr, smallest)
+            implicit none
+            integer, intent(in) :: arr(:)
+            integer, intent(out) :: smallest
+            integer :: i
+
+            smallest = arr(1)
+            do i = 2, size(arr)
+                if (arr(i) < smallest) then
+                    smallest = arr(i)
+                end if
+            end do
+        end subroutine findSmallest
+
+        ! Subprogram to find the largest value in the array
+        subroutine findLargest(arr, largest)
+            implicit none
+            integer, intent(in) :: arr(:)
+            integer, intent(out) :: largest
+            integer :: i
+
+            largest = arr(1)
+            do i = 2, size(arr)
+                if (arr(i) > largest) then
+                    largest = arr(i)
+                end if
+            end do
+        end subroutine findLargest
+
+        ! Subprogram to find the mode of the array
+        subroutine mode(arr, mode_val)
+            implicit none
+            integer, intent(in) :: arr(:)
+            integer, intent(out) :: mode_val
+            integer :: i, j, count, max_count, candidate
+
+            max_count = 0
+            do i = 1, size(arr)
+                count = 0
+                do j = 1, size(arr)
+                    if (arr(j) == arr(i)) then
+                        count = count + 1
+                    end if
+                end do
+                if (count > max_count) then
+                    max_count = count
+                    candidate = arr(i)
+                end if
+            end do
+            mode_val = candidate
+        end subroutine mode
